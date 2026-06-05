e0d5be3815c9a6ccd7696001d3fdc4d5c310d562
Shota Matsuda <shota@takram.com>
2026-06-05 13:23:40 +0900

    Remove workaround

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -495,11 +495,11 @@ async function checkFile( ctx, failedScreenshots, cleanPage, isMakeScreenshot, f
 
 				throw new Error( `Error happened while rendering file ${ file }: ${ e }` );
 
-			} /* else { // This can mean that the example doesn't use requestAnimationFrame loop
+			} else { // This can mean that the example doesn't use requestAnimationFrame loop
 
 				console.yellow( `Render timeout exceeded in file ${ file }` );
 
-			} */ // TODO: fix this
+			}
 
 		}
 
